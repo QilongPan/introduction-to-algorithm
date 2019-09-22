7bad3f1f6a6667d5cd8221345279186e12467923
QilongPan <3102377627@qq.com>
2019-09-22 23:22:51 +0800

    study

--- sort/radix_sort.py
+++ sort/radix_sort.py
@@ -4,7 +4,25 @@
 # @Author  : QilongPan 
 # @Email   : 3102377627@qq.com
 
-def radix_sort(arr):
+'''
+从低位到高位对数的每列排序
+'''
+def radix_sort(arr,radix=10):
     max_num = max(arr)
     for i in range(len(str(max_num))):
-        
+    	bucket = [[] for i in range(radix)]
+    	print(arr)
+    	for value in arr:
+    		print(value)
+    		print(value%(radix ** (i+1))//(radix**i))
+    		bucket[value%(radix ** (i+1))//(radix**i)].append(value)
+    	print(bucket)
+    	del arr[:]
+    	for buc in bucket:
+    		arr.extend(buc)
+    print(arr)
+    	
+
+arr = [30,45,22,72,9,12,26,80,22,43,1]
+radix_sort(arr)
+print(arr)
